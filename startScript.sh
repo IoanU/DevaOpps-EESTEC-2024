7581ded948c7906c2e4b5cee188acf0be47b8f8c
IoanU <ioan.ursescu1@gmail.com>
2024-11-09 15:20:51 +0200

    Update startScript.sh

--- startScript.sh
+++ startScript.sh
@@ -6,6 +6,11 @@ python3 ./source/extract_features.py
 # Step 2: Train the model using the extracted features
 python3 ./source/train_model.py
 
+# Step 3.0: Extract TCP payload from pcap file and convert to json file by decoding bytes
+python3 ./source/convert_to_json.py
+
+# New files are stored in folder: /InputData/test.json
+
 # Step 3: Classify test data using the trained model
 python3 ./source/classify.py
 
